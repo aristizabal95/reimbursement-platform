1e7808e9842cddaae08424a05c720f6204fc3c4e
Andres Gonzalez <andres.gonzalez@factored.ai>
2024-04-18 20:04:37 -0500

    Simplify navbar!

--- reimbursment-admin/src/components/nav/NavBar.jsx
+++ reimbursment-admin/src/components/nav/NavBar.jsx
@@ -3,22 +3,11 @@ import './styleNav.css'
 
 const NavBar = () => {
   return (
-    <header className='navheader'>
-        <section className='title-menu'>
-            <h1>Welcome</h1>
-            <button className='hamburger-button'>
-                <div className='hamburger'></div>
-            </button>
-        </section>
-
-        <nav className='navbar'>
-            <ul>
-                <li>home</li>
-                <li>create</li>
-                <li>other thing</li>
-            </ul>
-        </nav>
-    </header>
+    <ul className='navheader'>
+        <li><a href="/">home</a></li>
+        <li><a href="/create">create</a></li>
+        <li><a href="/invoices">invoices</a></li>
+    </ul>
   )
 }
 
