7f39b094a5ce46ac90538fdd465a57a21b843b63
Andres Gonzalez <andres.gonzalez@factored.ai>
2024-04-24 19:04:05 -0500

    Add preview image on new expense

--- reimbursment-admin/src/components/InvoiceForm/InvoiceForm.jsx
+++ reimbursment-admin/src/components/InvoiceForm/InvoiceForm.jsx
@@ -1,32 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './invoiceform.css'
 
 const InvoiceForm = ({click = false, submitFun}) => {
-  return (
+    
+    const [imagePath, setImagePath] = useState('');
+    const handleFileSelect = (e) => {
+        var file = e.target.files[0];
+        var reader = new FileReader();
+        reader.addEventListener("load", () => {
+            const src = reader.result.replace('[data:*/*;base64,]', '');
+            setImagePath(src);
+        }, false);
+        reader.readAsDataURL(file);
+        setToggleLabel(!toggleLabel);
+    }    
+
+    return (
     <form onSubmit= {submitFun} className="invoiceForm" style={{display: click ? "flex": "none"}}>
             <div className="image-upload">
-                <label htmlFor="file-input">
+                <img className="invoice-preview" src={imagePath}></img>
+                <label htmlFor="invoice-file">
                     <p> Upload File</p>
                 </label>
-                <input id="file-input" type="file" accept='image/*'/>
+                <input id="invoice-file" type="file" accept='image/*' onChange={handleFileSelect} required/>
             </div>
-            <p className='invoice-question'>
-                <label htmlFor="amnt">Amount</label>
-                <input type="tel" id="amnt" name="annt" required></input>
-            </p>
             <p className='invoice-question'>
                 <label htmlFor="vendor">Vendor</label>
                 <input type="text" id="vendor" name="vendor" required></input>
             </p>
             <p className='invoice-question'>
-                <label htmlFor="currency">Currency</label>
-                <select name='currency' id='currency'>
+                <label htmlFor="amnt">Amount</label>
+                <input type="tel" id="amnt" name="annt" required></input>
+                <select name='currency' id='currency' defaultValue='USD'>
                     <option value='COP'>COP</option>
-                    <option value='USD' selected="selected">USD</option>
+                    <option value='USD'>USD</option>
                     <option value='ARG'>ARG</option>
                 </select>
             </p>
-            <input type="submit"></input>
+            <button className="submit-expense" type="submit">Add expense</button>
         </form>
   )
 }
