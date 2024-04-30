3b9bef7f1a634e24301e83cf437256fb5709caae
Andres Gonzalez <andres.gonzalez@factored.ai>
2024-04-30 11:50:23 -0500

    using axios to fetch data.

--- reimbursment-admin/src/components/utils.jsx
+++ reimbursment-admin/src/components/utils.jsx
@@ -1,15 +1,16 @@
 import { createContext, useState } from "react";
+import axios from 'axios'
 
 export const fetchData = async (path, setter) => {
     try {
-        const data = await fetch(path);
-        const resp = await data.json();
-        setter(resp);
+        const resp = await axios.get(path, 
+            {headers: { 'Content-Type': 'application/json'}})
+        setter(resp.data);
     } catch (error) {
-        console.log(`Error while loading data from ${path}`, error)
+        console.log(`Error while loading data from ${path}`)
     }
 }
- 
+
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({children}) => {
