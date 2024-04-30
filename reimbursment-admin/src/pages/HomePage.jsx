4b0667af144f5f89844a815d2989e75bc1985cdf
Andres Gonzalez <andres.gonzalez@factored.ai>
2024-04-30 11:15:29 -0500

    Revert homepage changes

--- reimbursment-admin/src/pages/HomePage.jsx
+++ reimbursment-admin/src/pages/HomePage.jsx
@@ -1,12 +1,8 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import ReimbursmentList from '../components/reimbursmentList/ReimbursmentList'
-import { AuthContext } from '../components/utils'
-import Login from '../components/Login/Login'
-
 
 const HomePage = () => {
-    const { auth } = useContext(AuthContext)
-    return auth == null ? <ReimbursmentList></ReimbursmentList> : <Login></Login>
+    return <ReimbursmentList></ReimbursmentList>
 }
 
 export default HomePage
