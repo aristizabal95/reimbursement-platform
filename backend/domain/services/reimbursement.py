540063f4ceda6e54de6977e7eee54531e0baae37
Andres Gonzalez <andres.gonzalez@factored.ai>
2024-05-13 20:40:49 -0500

    Deal with no result from sql

--- backend/domain/services/reimbursement.py
+++ backend/domain/services/reimbursement.py
@@ -2,13 +2,14 @@ from fastapi import Request, Depends
 import dto.request as req
 import dto.response as res
 from infrastructure import sql
+from typing import List, Union
 import logging
 
 logger = logging.getLogger(__name__)
 
-async def user_reimbursements(user_id: str, r: Request) -> res.Reimbursement:
+async def user_reimbursements(user_id: str, r: Request) -> List[Union[res.Reimbursement, None]]:
     result = await sql.user_reimbursement(r.app.state.db, user_id)
-    return [res.Reimbursement(**r) for r in result]
+    return [res.Reimbursement(**r) for r in result if len(result) != 0]
 
 async def add_reimbursements(form: req.Reimbursment, r: Request):
     result = await sql.add_reimbursement(r.app.state.db, **form.model_dump())
