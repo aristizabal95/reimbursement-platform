d787aaf5111ee201ea408dc5f3a449c77d771971
Andres Gonzalez <andres.gonzalez@factored.ai>
2024-05-22 15:05:39 -0500

    Fix total in summary component.

--- Frontend/src/components/Summary.jsx
+++ Frontend/src/components/Summary.jsx
@@ -1,48 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import './Summary.css'
+import React, { useEffect, useState } from "react";
+import "./Summary.css";
 
-const Summary = ({detail = [], onSubmit}) => {
-    const defaultTotal = (detail.length != 0 ? detail.reduce((acc, v) => acc + v.amnt, 0): 0);
-    var amountsInit = (detail.length != 0 ? detail.map( el => ({k: `input${el.id}`, v: el.amnt + 0})): [{}]);
-    const [amounts, setAmounts] = useState(amountsInit)
-    const [total, setTotal] = useState(defaultTotal);
-    
-    const TotalElement = (
-    <div className='summarytotal'>
-        <p>{total} <span>COP</span></p>
-    </div>);
+const Summary = ({ detail = [], onSubmit }) => {
+  const defaultTotal =
+    detail.length != 0 ? detail.reduce((acc, v) => acc + v.amnt, 0) : 0;
+  var amountsInit =
+    detail.length != 0
+      ? detail.map((el) => ({ k: `input${el.invoice_id}`, v: el.amount + 0 }))
+      : [{}];
+  const [amounts, setAmounts] = useState(amountsInit);
+  const [total, setTotal] = useState(defaultTotal);
 
-    const changeInput = (e) => {
-        const key = e.target.id;
-        const newVal = +e.target.value;
-        setAmounts((prevState) => {
-            return prevState.map( el => (el.k == key ? {k: key, v: newVal}:el));
-        });
-    }
+  const TotalElement = (
+    <div className="summarytotal">
+      <p>
+        {total} <span>COP</span>
+      </p>
+    </div>
+  );
 
-    useEffect( () => {
-        const newTotal = amounts.reduce((acc, el) => acc + el.v, 0)
-        setTotal(newTotal);
-    }, [amounts])
+  const changeInput = (e) => {
+    //TODO: sum with a same currency.
+    const key = e.target.invoice_id;
+    const newVal = +e.target.value;
+    console.log(e.target.value);
+    setAmounts((prevState) => {
+      return prevState.map((el) => (el.k == key ? { k: key, v: newVal } : el));
+    });
+  };
 
-    return (
-    <form className='summaryform' onSubmit={onSubmit}>
-        {
-        (detail.length != 0 ? detail.map( (el) => {
-            return (
-                <div className='summaryitem' id={`summary${el.id}`}>
-                    <p>{el.vendor}</p>
-                    <input type="tel" id={`input${el.id}`} defaultValue={el.amnt} onChange={changeInput}></input>
-                    <span>{el.currency}</span>
-                </div>
-            );
-        }
-        ): <></>)
-        }
-        {TotalElement}
-        <button className="approve" type="submit">Approve</button>
+  useEffect(() => {
+    const newTotal = amounts.reduce((acc, el) => acc + el.v, 0);
+    setTotal(newTotal);
+  }, [amounts]);
+
+  return (
+    <form className="summaryform" onSubmit={onSubmit}>
+      {detail.length != 0 ? (
+        detail.map((el) => {
+          return (
+            <div className="summaryitem" id={`summary${el.invoice_id}`}>
+              <p>{el.vendor}</p>
+              <input
+                type="tel"
+                id={`input${el.invoice_id}`}
+                defaultValue={el.amount}
+                onChange={changeInput}
+              ></input>
+              <span>{el.currency}</span>
+            </div>
+          );
+        })
+      ) : (
+        <></>
+      )}
+      {TotalElement}
+      <button className="approve" type="submit">
+        Approve
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default Summary
+export default Summary;
