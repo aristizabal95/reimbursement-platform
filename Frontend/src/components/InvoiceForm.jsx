4b75747c53fffc546f8b0abcfa221cdabbc7cf61
Andres Gonzalez <andres.gonzalez@factored.ai>
2024-06-03 13:59:32 -0500

    Add spinner!

--- Frontend/src/components/InvoiceForm.jsx
+++ Frontend/src/components/InvoiceForm.jsx
@@ -5,9 +5,11 @@ import cameraIcon from "../assets/camera-icon.svg";
 import SuccessButton from "./SuccessButton.jsx";
 import CurrencySelector from "./CurrencySelector.jsx";
 import axios from "../api/axios.js";
+import Spinner from "./Spinner.jsx";
 
 const InvoiceForm = () => {
   const { reimbursementId, eventId } = useParams();
+  const [isLoading, setLoading] = useState(false);
   const [placeholder, setPlaceHolder] = useState({});
   const [imagePath, setImagePath] = useState("");
   const [expenseList, setExpenseList] = useState([]);
@@ -26,10 +28,12 @@ const InvoiceForm = () => {
         setImagePath(src);
         const formData = new FormData();
         formData.append("file", file);
+        setLoading(true);
         const resp = await axios.post("/invoices/invoices/parser", formData, {
           headers: { "Content-Type": "multipart/form-data" },
         });
         setPlaceHolder(resp.data);
+        setLoading(false);
       },
       false,
     );
@@ -94,69 +98,82 @@ const InvoiceForm = () => {
           />
         </label>
       </div>
-      <p className="flex flex-col justify-start">
-        <label htmlFor="expenseId" className="text-darkBlack700">
-          Expense
-        </label>
-        <select
-          name="expenseId"
-          id="expenseId"
-          className="m-1 pt-1 border rounded-[20px] bg-white pl-1.5"
-        >
-          {expenseList.map((e) => {
-            return <option value={e.id}>{e.name}</option>;
-          })}
-        </select>
-      </p>
-      <p className="flex flex-col justify-start">
-        <label htmlFor="dte" className="text-darkBlack700">
-          Date
-        </label>
-        <input
-          className="bg-white border rounded-[20px] m-1 pt-1 pl-1.5 bg-white"
-          type="date"
-          id="dte"
-          name="dte"
-          value={placeholder ? placeholder.date : ""}
-          required
-        ></input>
-      </p>
-      <p className="flex flex-col justify-start">
-        <label htmlFor="vendor" className="text-darkBlack700">
-          Vendor
-        </label>
-        <input
-          className="border rounded-[20px] m-1 pt-1 pl-1.5 bg-white"
-          type="text"
-          id="vendor"
-          name="vendor"
-          value={placeholder ? placeholder.vendor : ""}
-          required
-        ></input>
-      </p>
-      <p className="flex flex-col justify-start">
-        <label htmlFor="amount" className="text-darkBlack700">
-          Amount
-        </label>
-        <input
-          className="border rounded-[20px] m-1 pt-1 pl-1.5 bg-white"
-          type="tel"
-          id="amount"
-          name="amount"
-          value={placeholder ? placeholder.total_amount : ""}
-          required
-        ></input>
-      </p>
-      <p className="flex flex-col justify-start">
-        <lable htmlFor="currency" className="text-darkBlack700">
-          Currency
-        </lable>
-        <CurrencySelector
-          name="currency"
-          className="m-1 pt-1 border rounded-[20px] bg-white pl-1.5"
-        />
-      </p>
-      <SuccessButton text="Add Invoice" className="p-2"></SuccessButton>
+      <div
+        className="flex felx-row justify-center"
+        style={{ display: isLoading ? "" : "none" }}
+      >
+        <Spinner></Spinner>
+      </div>
+      <div
+        style={{
+          display:
+            isLoading | (Object.keys(placeholder).length == 0) ? "none" : "",
+        }}
+      >
+        <p className="flex flex-col justify-start">
+          <label htmlFor="expenseId" className="text-darkBlack700">
+            Expense
+          </label>
+          <select
+            name="expenseId"
+            id="expenseId"
+            className="m-1 pt-1 border rounded-[20px] bg-white pl-1.5"
+          >
+            {expenseList.map((e) => {
+              return <option value={e.id}>{e.name}</option>;
+            })}
+          </select>
+        </p>
+        <p className="flex flex-col justify-start">
+          <label htmlFor="dte" className="text-darkBlack700">
+            Date
+          </label>
+          <input
+            className="bg-white border rounded-[20px] m-1 pt-1 pl-1.5 bg-white"
+            type="date"
+            id="dte"
+            name="dte"
+            value={placeholder.date}
+            required
+          ></input>
+        </p>
+        <p className="flex flex-col justify-start">
+          <label htmlFor="vendor" className="text-darkBlack700">
+            Vendor
+          </label>
+          <input
+            className="border rounded-[20px] m-1 pt-1 pl-1.5 bg-white"
+            type="text"
+            id="vendor"
+            name="vendor"
+            value={placeholder.vendor}
+            required
+          ></input>
+        </p>
+        <p className="flex flex-col justify-start">
+          <label htmlFor="amount" className="text-darkBlack700">
+            Amount
+          </label>
+          <input
+            className="border rounded-[20px] m-1 pt-1 pl-1.5 bg-white"
+            type="tel"
+            id="amount"
+            name="amount"
+            value={placeholder.total_amount}
+            required
+          ></input>
+        </p>
+        <p className="flex flex-col justify-start">
+          <lable htmlFor="currency" className="text-darkBlack700">
+            Currency
+          </lable>
+          <CurrencySelector
+            name="currency"
+            className="m-1 pt-1 border rounded-[20px] bg-white pl-1.5"
+          />
+        </p>
+        <SuccessButton text="Add Invoice" className="p-2"></SuccessButton>
+      </div>
     </form>
   );
 };
