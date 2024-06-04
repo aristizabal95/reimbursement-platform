0268ebf05322429ff0c02bbbcf4e444d7962541d
Andres Gonzalez <andres.gonzalez@factored.ai>
2024-06-03 22:58:34 -0500

    Responsive navbar. Not yet but better!

--- Frontend/src/Layouts/MainLayout.jsx
+++ Frontend/src/Layouts/MainLayout.jsx
@@ -4,7 +4,7 @@ import NavBar from "../components/NavBar";
 
 const MainLayout = () => {
   return (
-    <section className="flex h-full w-full">
+    <section className="flex flex-col h-full w-full md:flex-row">
       <NavBar></NavBar>
       <Outlet></Outlet>
     </section>
--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -8,26 +8,18 @@ import menu from "../assets/menu.svg";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  const linkMargin = "mb-5";
   return (
-    <nav className="shrink-0 p-4 mr-1 flex flex-col justify-between items-center left-0 top-0 shadow-nav h-[100vh] w-[78px]">
-      <section className="flex flex-col items-center content-evenly">
-        <img src={factorLogo} className="mb-8 h-[42px] w-[36px]"></img>
-        <Link className={linkMargin} to="/task">
-          <img src={taskIcon}></img>
-        </Link>
-        <Link className={linkMargin} to="/">
-          <img src={receiptIcon}></img>
-        </Link>
-        <Link className={linkMargin} to="/add-event">
-          <img src={diamond}></img>
-        </Link>
-      </section>
-      <section className="flex-reversed">
-        <Link className={linkMargin} to="/">
-          <img src={menu}></img>
-        </Link>
-      </section>
+    <nav className="flex flex-row h-[55px] md:h-[100vh] md:flex-col justify-evenly md:content-evenly items-center shadow-nav">
+      <img src={factorLogo} className="left-0 md:top-0 h-[42px] w-[36px]"></img>
+      <Link className="text-blue-600 underline" to="/task">
+        Task
+      </Link>
+      <Link className="text-blue-600 underline" to="/">
+        Home
+      </Link>
+      <Link className="text-blue-600 underline" to="/add-event">
+        New event
+      </Link>
     </nav>
   );
 };
